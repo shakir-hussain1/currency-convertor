c0c7deb2304e3b203121a028dd9588821d564540
marketplace <sadsain01@gmail.com>
2024-04-07 05:23:08 +0500

    created currency converter

---
+++ index.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bgYellow.underline.black("Welcome to Currency Converter App"));
+const currency = {
+    USD: 1, //base currency
+    EUR: 0.91,
+    GBP: 0.76,
+    iNR: 74.57,
+    PKR: 280
+};
+let userAnswer = await inquirer.prompt([
+    {
+        name: "from",
+        type: "list",
+        message: (chalk.red.underline("Enter from Currency:")),
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: "to",
+        type: "list",
+        message: (chalk.green.underline("Enter to Currency:")),
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: "amount",
+        type: "number",
+        message: (chalk.magenta.underline("Enter Your Amount:"))
+    }
+]);
+let fromAmount = currency[userAnswer.from]; //exchange rate
+let toAmount = currency[userAnswer.to]; //exchange rate
+let amount = userAnswer.amount;
+let baseAmount = amount / fromAmount; //USD base currency // 4
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
---
+++ index.ts
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+import chalk from "chalk"
+
+console.log(chalk.bgYellow.underline.black("Welcome to Currency Converter App"))
+const currency:any =
+{
+    USD: 1, //base currency
+    EUR: 0.91,
+    GBP: 0.76,
+    iNR: 74.57,
+    PKR: 280
+};
+
+let userAnswer = await inquirer.prompt(
+    [
+        {
+            name: "from",
+            type: "list",
+            message: (chalk.red.underline("Enter from Currency:")),
+            choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+        },
+        {
+            name: "to",
+            type: "list",
+            message: (chalk.green.underline("Enter to Currency:")),
+            choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+        },
+        {
+            name: "amount",
+            type: "number",
+            message: (chalk.magenta.underline("Enter Your Amount:"))
+        }
+    ]
+)
+
+let fromAmount = currency[userAnswer.from] //exchange rate
+let toAmount = currency[userAnswer.to] //exchange rate
+let amount = userAnswer.amount
+let baseAmount = amount / fromAmount //USD base currency // 4
+let convertedAmount = baseAmount * toAmount
+console.log(convertedAmount);
